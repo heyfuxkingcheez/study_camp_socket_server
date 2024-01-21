15b0e71ebe3be3063dedaaa3675d016ccdd1f6b2
da <dain940209@gmail.com>
2024-01-22 08:08:49 +0900

    ✨ feat : join space

--- src/socket.js
+++ src/socket.js
@@ -5,7 +5,8 @@ export default function socket(socketIo) {
     console.log(socket.id, 'user connected');
     const userdata = {
       id: socket.id,
-      nickname: '닉네임',
+      spaceId: 0,
+      nickName: '닉네임',
       x: 1,
       y: 1,
       isSit: false,
@@ -18,14 +19,21 @@ export default function socket(socketIo) {
       const userdata = userMap.get(socket.id);
       userMap.delete(socket.id);
 
-      socketIo.sockets.emit('leavSpace', userdata);
+      if (userdata.spaceId) {
+        socketIo.sockets
+          .to(`space ${userdata.spaceId}`)
+          .emit('leaveSpace', userdata);
+      }
     });
 
     socket.on('joinSpace', (data) => {
       const userdata = userMap.get(socket.id);
+      userdata.nickName = data.nickName;
+      userdata.spaceId = data.spaceId;
       userdata.x = data.x;
       userdata.y = data.y;
       userMap.set(socket.id, userdata);
+      socket.join(`space ${data.spaceId}`);
       socketIo.sockets.emit('joinSpacePlayer', userdata);
       socket.emit('spaceUsers', [...userMap.values()]);
     });
@@ -35,20 +43,25 @@ export default function socket(socketIo) {
       userdata.x = data.x;
       userdata.y = data.y;
       userMap.set(data.id, userdata);
-      socketIo.sockets.emit('movePlayer', userdata);
+      socketIo.sockets
+        .to(`space ${userdata.spaceId}`)
+        .emit('movePlayer', userdata);
     });
 
     socket.on('sit', (data) => {
       const userdata = userMap.get(data.id);
       userdata.isSit = data.isSit;
       userMap.set(data.id, userdata);
-      socketIo.sockets.emit('sitPlayer', userdata);
+      socketIo.sockets
+        .to(`space ${userdata.spaceId}`)
+        .emit('sitPlayer', userdata);
     });
 
     socket.on('chat', (data) => {
       // id, message
-      socketIo.sockets.emit('chatPlayer', {
+      socketIo.sockets.to(`space ${userdata.spaceId}`).emit('chatPlayer', {
         id: socket.id,
+        nickName: data.nickName,
         message: data.message,
       });
     });

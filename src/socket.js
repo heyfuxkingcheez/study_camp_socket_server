b75866a5fb8b6ae1bf11879cc16a61e3f3761a6d
da <dain940209@gmail.com>
2024-01-25 16:14:33 +0900

    ✨ feat : change skin

--- src/socket.js
+++ src/socket.js
@@ -7,8 +7,15 @@ export default function socket(socketIo) {
       id: socket.id,
       spaceId: 0,
       nickName: '닉네임',
+      memberId: 0,
       x: 1,
       y: 1,
+      skin: 0,
+      face: 0,
+      hair: 0,
+      hair_color: 0,
+      clothes: 0,
+      clothes_color: 0,
       isSit: false,
     };
     userMap.set(socket.id, userdata);
@@ -17,25 +24,37 @@ export default function socket(socketIo) {
     socket.on('disconnect', () => {
       console.log(socket.id, ' user disconnected');
       const userdata = userMap.get(socket.id);
-      userMap.delete(socket.id);
 
       if (userdata.spaceId) {
         socketIo.sockets
           .to(`space ${userdata.spaceId}`)
           .emit('leaveSpace', userdata);
       }
+      userMap.delete(socket.id);
     });
 
     socket.on('joinSpace', (data) => {
       const userdata = userMap.get(socket.id);
       userdata.nickName = data.nickName;
       userdata.spaceId = data.spaceId;
+      userdata.memberId = data.memberId;
       userdata.x = data.x;
       userdata.y = data.y;
+      userdata.skin = data.skin;
+      userdata.face = data.face;
+      userdata.hair = data.hair;
+      userdata.hair_color = data.hair_color;
+      userdata.clothes = data.clothes;
+      userdata.clothes_color = data.clothes_color;
       userMap.set(socket.id, userdata);
       socket.join(`space ${data.spaceId}`);
-      socketIo.sockets.emit('joinSpacePlayer', userdata);
-      socket.emit('spaceUsers', [...userMap.values()]);
+      socketIo.sockets
+        .to(`space ${data.spaceId}`)
+        .emit('joinSpacePlayer', userdata);
+      const spaceUsers = [...userMap.values()].filter(
+        (user) => user.spaceId === data.spaceId,
+      );
+      socket.emit('spaceUsers', spaceUsers);
     });
 
     socket.on('move', (data) => {
@@ -57,6 +76,20 @@ export default function socket(socketIo) {
         .emit('sitPlayer', userdata);
     });
 
+    socket.on('updateSkin', (data) => {
+      const userdata = userMap.get(data.id);
+      userdata.skin = data.skin;
+      userdata.face = data.face;
+      userdata.hair = data.hair;
+      userdata.hair_color = data.hair_color;
+      userdata.clothes = data.clothes;
+      userdata.clothes_color = data.clothes_color;
+      userMap.set(data.id, userdata);
+      socketIo.sockets
+        .to(`space ${userdata.spaceId}`)
+        .emit('updateSkinPlayer', userdata);
+    });
+
     socket.on('chat', (data) => {
       // id, message
       socketIo.sockets.to(`space ${userdata.spaceId}`).emit('chatPlayer', {
@@ -67,7 +100,7 @@ export default function socket(socketIo) {
     });
     //특정 플레이어에게 메세지를 보내야한다.
     socket.on('directMessageToPlayer', (data) => {
-      socketIo.sockets.to(data.getterId).emit("directMessage", {
+      socketIo.sockets.to(data.getterId).emit('directMessage', {
         senderId: data.senderId,
         message: data.message,
       });

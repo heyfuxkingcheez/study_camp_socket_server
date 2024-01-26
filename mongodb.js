1935549c30342099e3134e70643417ef8ed77911
da <dain940209@gmail.com>
2024-01-27 03:29:26 +0900

    🔨 update : mongodb env

--- mongodb.js
+++ mongodb.js
@@ -1,15 +1,18 @@
 import mongoose from 'mongoose';
 
 const connectToDatabase = () => {
-  mongoose.connect(`mongodb+srv://studyCampOwner1234:${process.env.ATLASPASSWORD}@studycamp.urrwmga.mongodb.net/study_camp?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose.connect(
+    `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PW}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
 
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'Connection error:'));
   db.once('open', function () {
-    console.log('MongoDB connected!',mongoose.connection.db.databaseName);
+    console.log('MongoDB connected!', mongoose.connection.db.databaseName);
   });
 
   return mongoose;
